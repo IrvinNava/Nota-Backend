f7c6bc8bbacbac89b29831b24d4839029c12b8a2
Irvin Nava <irvinxf@gmail.com>
2023-12-07 17:17:17 -0600

    Se agrega modal para correo de contratación en detalle de speaker

--- resources/views/homeNota/speaker.blade.php
+++ resources/views/homeNota/speaker.blade.php
@@ -26,7 +26,7 @@
                <div class="col-md-3">
                   <div class="sticky-top" style="top: 15px;">
                      <img src="{{ $speaker->speaker_photo }}" alt="Mark Travis" class="img-fluid">
-                     <a href="" class="btn btn-lg btn-purple btn-block rounded-pill px-5 mt-3 text-center">Hire this Speaker</a>
+                     <a href="" class="btn btn-lg btn-purple btn-block rounded-pill px-5 mt-3 text-center" data-bs-toggle="modal" data-bs-target="#staticBackdrop">Hire this Speaker</a>
                   </div>
                </div>
                <div class="col-md-9 speaker-detail-container">
@@ -47,13 +47,13 @@
                         @endforeach
 
                         @if($speaker->topics !=null)
-                           <? $topics = explode(",", $speaker->topics); ?>
-                           @foreach ($topics as $topic)
-                           <a href="javascript:void(0)" class="nota-tag"><?= \App\Topics::getName($topic) ?></a>
-                           @endforeach
+                        <? $topics = explode(",", $speaker->topics); ?>
+                        @foreach ($topics as $topic)
+                        <a href="javascript:void(0)" class="nota-tag"><?= \App\Topics::getName($topic) ?></a>
+                        @endforeach
                         @endif
 
-                      
+
                         <!--<a href="" class="nota-tag">DEI Elementals</a>
                         <a href="" class="nota-tag">Inclusive Leadership</a>
                         <a href="" class="nota-tag">Black History Month</a>
@@ -144,7 +144,7 @@
             </div>
          </div>
          <div class="d-flex justify-content-center">
-            <a href="" class="btn btn-lg btn-purple rounded-pill px-5 mt-5">Hire this Speaker</a>
+            <a href="" class="btn btn-lg btn-purple rounded-pill px-5 mt-5" data-bs-toggle="modal" data-bs-target="#staticBackdrop">Hire this Speaker</a>
          </div>
       </section>
       @if($experiences)
@@ -203,6 +203,86 @@
          </div>
       </section>
       @endif
+
+      <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+         <div class="modal-dialog modal-lg modal-dialog-centered">
+            <div class="modal-content">
+               <div class="modal-body p-0">
+
+                  <div class="row g-0">
+                     <div class="col-md-6 nota-bg-light">
+                        <div class="d-flex flex-column justify-content-between p-5 h-100">
+                           <div>
+                              <img width="120" src="https://notainclusion.com/wp-content/uploads/2023/06/logo.png" alt="Notainclusion Logo" class="">
+                              <h2 class="speaker-modal-title mt-5">¡Great, you want to hire <span>{{ $speaker->first_name }} {{ $speaker->last_name }}!</span></h2>
+                           </div>
+                           <img src="https://notainclusion.com/wp-content/uploads/2023/07/nota_portada.png" alt="Notainclusion s peakers" class="img-fluid">
+                        </div>
+                     </div>
+                     <div class="col-md-6">
+
+                        <div class="d-flex justify-content-end pt-4 px-4">
+                           <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div class="pb-3 px-5">
+                           <form id="phpcontactformSpeaker" class="php-email-form2" data-recaptcha-site-key="6LeJtWscAAAAADaftEpstS3-Vir26QO1cysjDewK">
+                              <div class="row">
+                                 <div class="form-group">
+                                    <label for="fullnameS" class="form-label">Full name</label>
+                                    <input type="text" placeholder="" id="fullnameS" name="fullname" class="form-control form-control-sm" required="">
+                                 </div>
+                                 <input type="hidden" name="speakers" id="speaker" class="form-control form-control-sm" value="{{ $speaker->first_name }} {{ $speaker->last_name }}">
+
+                                 <div class="form-group">
+                                    <label for="companyS" class="form-label">Company name</label>
+                                    <input type="text" class="form-control form-control-sm" name="company" id="companyS" required="">
+                                 </div>
+
+                                 <div class="form-group">
+                                    <label for="emailS" class="form-label">Email</label>
+                                    <input type="email" name="email" class="form-control form-control-sm" id="emailS" required="">
+                                 </div>
+                                 <div class="form-group">
+                                    <label for="messageS" class="form-label">Your message</label>
+                                    <textarea class="form-control form-control-sm mb-0" name="message" id="messageS" rows="4" required="">I want to hire {{ $speaker->first_name }} {{ $speaker->last_name }}</textarea>
+                                 </div>
+
+                                 <div>
+                                    <div class="loading mt-3"><i class="fa-solid fa-spinner fa-spin-pulse"></i> Loading</div>
+                                    <div class="error-message mt-3">
+                                       <div class="alert alert-danger" role="alert">
+                                          ¡Oh! It looks like something has gone wrong. You can try again.
+                                       </div>
+                                    </div>
+                                    <div class="sent-message mt-3">
+                                       <div class="alert alert-success" role="alert">
+                                          <i class="fa-solid fa-check me-1"></i> Your message has been sent. Thank you!
+                                       </div>
+                                    </div>
+                                 </div>
+                                 
+                              </div>
+                           </form>
+
+                           <div class="d-flex justify-content-end mt-3 mb-5">
+                              <button type="button" class="btn btn-sm btn-light rounded-pill px-3" data-bs-dismiss="modal">Cancel</button>
+                              <div id="contact-btnS" class="ms-2"><button type="submit" class="btn btn-sm btn-purple rounded-pill px-4 popbtn">Submit</button></div>
+                           </div>
+
+                           <div class="modal-recaptcha-text">
+                              <p>This site is protected by reCAPTCHA and the Google <a href="https://policies.google.com/privacy">Privacy Policy</a> and <a href="https://policies.google.com/terms">Terms</a> apply.</p>
+                           </div>
+
+                        </div>
+                     </div>
+                  </div>
+
+
+               </div>
+            </div>
+         </div>
+      </div>
+
    </main>
 
    @include('layoutPublic.footer')
